c0ccb7821db1b2ee1c73acca2ca0ff9805c693f1
alanir2 <156774124+alanir2@users.noreply.github.com>
2024-09-01 14:57:49 +0300

    Update project.py
    new commit

--- project.py
+++ project.py
@@ -1,40 +1,81 @@
 import os
-import json
 import pandas as pd
 
-class PriceMachine():
-    
+
+class PriceMachine:
+
     def __init__(self):
         self.data = []
-        self.result = ''
         self.name_length = 0
-    
-    def load_prices(self, file_path=''):
+
+    def load_prices(self, directory=''):
         '''
-            Сканирует указанный каталог. Ищет файлы со словом price в названии.
-            В файле ищет столбцы с названием товара, ценой и весом.
-            Допустимые названия для столбца с товаром:
-                товар
-                название
-                наименование
-                продукт
-                
-            Допустимые названия для столбца с ценой:
-                розница
-                цена
-                
-            Допустимые названия для столбца с весом (в кг.)
-                вес
-                масса
-                фасовка
+        Сканирует указанный каталог. Ищет файлы со словом price в названии.
+        В файле ищет столбцы с названием товара, ценой и весом.
+        Допустимые названия для столбца с товаром:
+            товар
+            название
+            наименование
+            продукт
+
+        Допустимые названия для столбца с ценой:
+            розница
+            цена
+
+        Допустимые названия для столбца с весом (в кг.):
+            вес
+            масса
+            фасовка
         '''
-        
+        for filename in os.listdir(directory):
+            if 'price' in filename.lower() and filename.endswith('.csv'):
+                file_path = os.path.join(directory, filename)
+                print(f'Загружаю файл: {file_path}')
+                data_frame = pd.read_csv(file_path, sep=',')
+
+                # Находим индексы нужных колонки
+                product_col, price_col, weight_col = self._search_product_price_weight(data_frame.columns)
+
+                for index, row in data_frame.iterrows():
+                    if product_col is not None and price_col is not None and weight_col is not None:
+                        self.name_length += 1
+                        self.data.append({
+                            'Номер': self.name_length,
+                            'Наименование': row[product_col],
+                            'Цена': row[price_col],
+                            'Фасовка': row[weight_col],
+                            'Файл': filename,
+                            'Цена за кг.': row[price_col] / row[weight_col] if row[weight_col] != 0 else 0
+                        })
+
+        # Сортировка данных по цене за кг
+        self.data = self.find_text(self.data)
+
+        # Присвоение номера каждому элементу после сортировки
+        self.name_length = len(self.data)
+        for idx, item in enumerate(self.data):
+            item['Номер'] = idx + 1  # Присваиваем номер начиная с 1
+
     def _search_product_price_weight(self, headers):
         '''
-            Возвращает номера столбцов
+        Возвращает индексы столбцов: продукт, цена, вес
         '''
+        product_col = price_col = weight_col = None
 
-    def export_to_html(self, fname='output.html'):
+        for header in headers:
+            if header.lower() in ['товар', 'название', 'наименование', 'продукт']:
+                product_col = header
+            elif header.lower() in ['цена', 'розница']:
+                price_col = header
+            elif header.lower() in ['вес', 'масса', 'фасовка']:
+                weight_col = header
+
+        return product_col, price_col, weight_col
+
+    def export_to_html(self, fname='my_output.html'):
+        '''
+        Экспортирует все данные в HTML формат.
+        '''
         result = '''
         <!DOCTYPE html>
         <html>
@@ -42,27 +83,50 @@ class PriceMachine():
             <title>Позиции продуктов</title>
         </head>
         <body>
-            <table>
+            <table border="1">
                 <tr>
                     <th>Номер</th>
-                    <th>Название</th>
+                    <th>Наименование</th>
                     <th>Цена</th>
                     <th>Фасовка</th>
                     <th>Файл</th>
                     <th>Цена за кг.</th>
                 </tr>
         '''
-    
+
+        for item in self.data:
+            result += f'''
+                <tr>
+                    <td>{item['Номер']}</td>
+                    <td>{item['Наименование']}</td>
+                    <td>{item['Цена']}</td>
+                    <td>{item['Фасовка']}</td>
+                    <td>{item['Файл']}</td>
+                    <td>{item['Цена за кг.']:.2f}</td>
+                </tr>
+            '''
+
+        result += '''
+            </table>
+        </body>
+        </html>
+        '''
+
+        with open(fname, 'w', encoding='utf-8') as file:
+            file.write(result)
+
     def find_text(self, text):
-        ...
+        '''
+        Получает текст и возвращает список позиций, содержащий этот текст в названии продукта.
+        '''
 
-    
-pm = PriceMachine()
-print(pm.load_prices())
+        return sorted(text, key=lambda x: x['Цена за кг.'])
 
-'''
-    Логика работы программы
-'''
-print('the end')
-print(pm.export_to_html())
 
+# Пример использования
+if __name__ == "__main__":
+    pm = PriceMachine()
+    pm.load_prices(os.getcwd())  # Замените на путь к вашей директории
+
+    pm.export_to_html('my_output.html')
+    print("Данные успешно экспортированы в my_output.html")
